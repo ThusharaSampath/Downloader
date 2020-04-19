3fbedd50b470b798b82f0e8bf2bef08df5aa745d
chamikagangul <pchamikagangul@gmail.com>
2020-04-19 21:50:04 +0530

    change db to backup

--- routes/search.js
+++ routes/search.js
@@ -12,13 +12,13 @@ var document = new Document();
 var drive = new Drive();
 
 router.get('/', async function (req, res, next) {
-    var searchtag = 'bad';
+    var searchtag = 'Money';
     res.end("1");
     await document.getVideos().then(async data => {
         //fs.writeFileSync('data.json',JSON.stringify(data));
         data.forEach(video => {
             if (video.files.name.includes(searchtag)) {
-                console.log(video.name);
+                console.log(video.files.name);
             }
         });
     });
