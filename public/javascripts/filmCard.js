76eae61d4017e1c1646303fa5a25675d81d0975d
ThusharaSampath <59133724+ThusharaSampath@users.noreply.github.com>
2020-04-21 23:59:54 +0530

    12

--- public/javascripts/filmCard.js
+++ public/javascripts/filmCard.js
@@ -1,5 +1,5 @@
 
-data = `{"1VQT1-F8mI4MZ77Z7bM6IlFaz0JeujMsn":{"id":"1VQT1-F8mI4MZ77Z7bM6IlFaz0JeujMsn","username":"downloadto2drive@gmail.com","name":"like.a.boss.2020.720p.bluray.x264.aac-[yts.mx].mp4","size":"803084572","thumbnail":"https://lh3.googleusercontent.com/NVMwlnGBG3ouUZRJ67g4hWiEmOwQWXJA0gyRZHSJLnP9PftWOO8q0sWO_ZD18pL2QYHe9ppzF40=s220","url_view":"https://drive.google.com/file/d/1VQT1-F8mI4MZ77Z7bM6IlFaz0JeujMsn/view?usp=drivesdk","url":"https://drive.google.com/uc?id=1VQT1-F8mI4MZ77Z7bM6IlFaz0JeujMsn&export=download","mimeType":"video/mp4","email":"downloadto2drive@gmail.com","owner":"download2 drive"},"1ynaQgK_VitxMqz4pN0oUICIm-0xV_8SI":{"id":"1ynaQgK_VitxMqz4pN0oUICIm-0xV_8SI","username":"hackerboy.8511@gmail.com","name":"john.wick.3.parabellum.2019.720p.bluray.hindi-english.x264-katmoviehd.nl.mkv","size":"1191180252","thumbnail":"https://lh3.googleusercontent.com/ZChMDuSjr5h_Fho1T3KcnGUx94ZlNIq54ZxQTZfwcGoKDVkm67uAsMfzjRJv_X0ZvNalDBQkAqo=s220","url_view":"https://drive.google.com/file/d/1ynaQgK_VitxMqz4pN0oUICIm-0xV_8SI/view?usp=drivesdk","url":"https://drive.google.com/uc?id=1ynaQgK_VitxMqz4pN0oUICIm-0xV_8SI&export=download","mimeType":"video/x-matroska","email":"hackerboy.8511@gmail.com","owner":"hacker boy"},"1-Pv8KhMakfgdA2q6P72dWf8YR3cbS4IN":{"id":"1-Pv8KhMakfgdA2q6P72dWf8YR3cbS4IN","username":"hackerboy.8511@gmail.com","name":"5. manually_payload_binding_with_any_apk_part_1.mp4","size":"153520996","thumbnail":"https://lh3.googleusercontent.com/guBIG3puoL6NVJjrZ1DeVx-QNGcKwg-plZWCGZtV1IKDaKfSmRUxNUxBh5MIvP_qj6sUp_kryIU=s220","url_view":"https://drive.google.com/file/d/1-Pv8KhMakfgdA2q6P72dWf8YR3cbS4IN/view?usp=drivesdk","url":"https://drive.google.com/uc?id=1-Pv8KhMakfgdA2q6P72dWf8YR3cbS4IN&export=download","mimeType":"video/mp4","email":"asoliya16@gmail.com","owner":"Prashant Asoliya"},"1L-HU-IE9oeeYjcHcLRJrMJIkyGnxG6u5":{"id":"1L-HU-IE9oeeYjcHcLRJrMJIkyGnxG6u5","username":"hackerboy.8511@gmail.com","name":"nikunj b .mp4","size":"46697524","thumbnail":"https://lh3.googleusercontent.com/rGInwhnPEK2F1gxDKcGI5c66TQnmVBCCSKSCQBJ6T4HTamLaZ0p_4Gv0tP2p4Okf4yLHRAgbUFk=s220","url_view":"https://drive.google.com/file/d/1L-HU-IE9oeeYjcHcLRJrMJIkyGnxG6u5/view?usp=drivesdk","url":"https://drive.google.com/uc?id=1L-HU-IE9oeeYjcHcLRJrMJIkyGnxG6u5&export=download","mimeType":"video/mp4","email":"hackerboy.8511@gmail.com","owner":"hacker boy"},"1mozRl_DBg2ntqTYLQRLQyT3VYf3Bvy1S":{"id":"1mozRl_DBg2ntqTYLQRLQyT3VYf3Bvy1S","username":"","name":"[kamigami] hunter x hunter - 115 [x264 1280x720 aac sub(ch,jp)].mkv","size":"108181709","thumbnail":"https://lh3.googleusercontent.com/XpXUhr4qe5Hpz2ehMKWD8TTIlOqQnIzMJ1w5iaJX-32EF-K_oSvZNBTE8rbwXfLmU0143UPAi7U=s220","url_view":"https://drive.google.com/a/coung.aobe.nl/file/d/1mozRl_DBg2ntqTYLQRLQyT3VYf3Bvy1S/view?usp=drivesdk","url":"https://drive.google.com/a/coung.aobe.nl/uc?id=1mozRl_DBg2ntqTYLQRLQyT3VYf3Bvy1S&export=download","mimeType":"video/x-matroska","email":"wr1092337@swccd.edu","owner":"Wayne Rosales"},"1G_LSXnwQ3TEc0ST2bRMRopA3a2bLNY04":{"id":"1G_LSXnwQ3TEc0ST2bRMRopA3a2bLNY04","username":"","name":"[kamigami] hunter x hunter - 114 [x264 1280x720 aac sub(ch,jp)].mkv","size":"241278978","thumbnail":"https://lh3.googleusercontent.com/SjpnT0HDolTHCAygnc675Y0GIgn8ZQAnYYMHp0HI33ga1OAVQzaTu89qc-ZANoRc2vVl34S59NE=s220","url_view":"https://drive.google.com/a/coung.aobe.nl/file/d/1G_LSXnwQ3TEc0ST2bRMRopA3a2bLNY04/view?usp=drivesdk","url":"https://drive.google.com/a/coung.aobe.nl/uc?id=1G_LSXnwQ3TEc0ST2bRMRopA3a2bLNY04&export=download","mimeType":"video/x-matroska","email":"wr1092337@swccd.edu","owner":"Wayne Rosales"}}`
+data = `{"1b8psGiOeATyQYjNAnLFehYlVu1VPQz_-":{"id":"1b8psGiOeATyQYjNAnLFehYlVu1VPQz_-","username":"","name":"[kamigami] hunter x hunter - 104 [x264 1280x720 aac sub(ch,jp)].mkv","size":"192183819","thumbnail":"https://lh3.googleusercontent.com/X8BPbQq6MGSqNRAe06kL8E1LFH35ual2wjClZeZ0HI8hn1f-p14y96JEECeU3qZQMEfv8Vtbdqs=s220","url_view":"https://drive.google.com/a/coung.aobe.nl/file/d/1b8psGiOeATyQYjNAnLFehYlVu1VPQz_-/view?usp=drivesdk","url":"https://drive.google.com/a/coung.aobe.nl/uc?id=1b8psGiOeATyQYjNAnLFehYlVu1VPQz_-&export=download","mimeType":"video/x-matroska","email":"wr1092337@swccd.edu","owner":"Wayne Rosales"},"1_n9XT_EpNVVA0QkJIFVwwftjQDI--4Qo":{"id":"1_n9XT_EpNVVA0QkJIFVwwftjQDI--4Qo","username":"","name":"[kamigami] hunter x hunter - 103 [x264 1280x720 aac sub(ch,jp)].mkv","size":"180231846","thumbnail":"https://lh3.googleusercontent.com/sVmZblp7-AK5w66w-3HR6KjpOASemo_Ce8as3OYJT21QO6gWe0NkFdZYtsloTjRbRvJSQZMtgyg=s220","url_view":"https://drive.google.com/a/coung.aobe.nl/file/d/1_n9XT_EpNVVA0QkJIFVwwftjQDI--4Qo/view?usp=drivesdk","url":"https://drive.google.com/a/coung.aobe.nl/uc?id=1_n9XT_EpNVVA0QkJIFVwwftjQDI--4Qo&export=download","mimeType":"video/x-matroska","email":"wr1092337@swccd.edu","owner":"Wayne Rosales"},"13Pn864zkt9NX1gz1RkUsJ38CF0ADZjbN":{"id":"13Pn864zkt9NX1gz1RkUsJ38CF0ADZjbN","username":"","name":"[kamigami] hunter x hunter - 102 [x264 1280x720 aac sub(ch,jp)].mkv","size":"173250018","thumbnail":"https://lh3.googleusercontent.com/aPzktbWHRQ25eMZItb1eF2gCAxYBwR6TQWyjPtpw_udga7fV_2QqWp83EExw-Ir9XgScrcpbbZs=s220","url_view":"https://drive.google.com/a/coung.aobe.nl/file/d/13Pn864zkt9NX1gz1RkUsJ38CF0ADZjbN/view?usp=drivesdk","url":"https://drive.google.com/a/coung.aobe.nl/uc?id=13Pn864zkt9NX1gz1RkUsJ38CF0ADZjbN&export=download","mimeType":"video/x-matroska","email":"wr1092337@swccd.edu","owner":"Wayne Rosales"},"11u-XhjjiEj7qr2KuopAAgelT_aBFdEDl":{"id":"11u-XhjjiEj7qr2KuopAAgelT_aBFdEDl","username":"","name":"[kamigami] hunter x hunter - 101 [x264 1280x720 aac sub(ch,jp)].mkv","size":"185115184","thumbnail":"https://lh3.googleusercontent.com/VJn64GTkjlizA6c4vj6_2r4tdCq1mMPpzj7l_2oKWK_wVk-V4e8AC4KPvHo-xoAqSfBuM14tSYk=s220","url_view":"https://drive.google.com/a/coung.aobe.nl/file/d/11u-XhjjiEj7qr2KuopAAgelT_aBFdEDl/view?usp=drivesdk","url":"https://drive.google.com/a/coung.aobe.nl/uc?id=11u-XhjjiEj7qr2KuopAAgelT_aBFdEDl&export=download","mimeType":"video/x-matroska","email":"wr1092337@swccd.edu","owner":"Wayne Rosales"}}`
 function test() {
   $.ajax('/search', {
     type: 'POST',  // http method
@@ -22,32 +22,77 @@ function test() {
 }
 
 
+// function makeAcard(Json) {
+//   html = `
+//   <div class="container column" style="background-image: url(${Json.thumbnail})">
+//   <div class="overlay">
+//     <div class = "items">
+//       <div class = "items head">
+//         <h7>${Json.name}</h7>
+//         <hr>
+//       </div>
+//       <div class = "items price">
+//         <p class="new">Dowunloads - </p>
+//         <p class="new">Rate - </p>
+//       </div>
+//       <div class="items cart">      
+//         <button type="button" class="btn btn-outline-info"><i class="fa fa-download"></i> Download</button>
+//         <button type="button" class="btn btn-outline-info"><i class="fa fa-book"></i> Watch</button>
+//       </div>
+//     </div>
+//   </div>
+//     <div>
+    
+    
+    
+//     </div>
+//   </div> 
+  
+//   `
+//   return html;
+// }
 function makeAcard(Json) {
   html = `
-  <div class="container column" style="background-image: url(${Json.thumbnail})">
-  <div class="overlay">
-    <div class = "items">
-      <div class = "items head">
-        <h7>${Json.name}</h7>
-        <hr>
-      </div>
-      <div class = "items price">
-        <p class="new">Dowunloads - </p>
-        <p class="new">Rate - </p>
-      </div>
-      <div class="items cart">      
-        <button type="button" class="btn btn-outline-info"><i class="fa fa-download"></i> Download</button>
-        <button type="button" class="btn btn-outline-info"><i class="fa fa-book"></i> Watch</button>
-      </div>
-    </div>
+  <figure class="card card--dark">
+  <div class="card__image-container">
+    <img src=${Json.thumbnail} alt="Umbreon" class="card__image">   
   </div>
-    <div>
-    
-    
-    
-    </div>
-  </div> 
   
+  <figcaption class="card__caption">
+    <h1 class="card__name">${Json.name}</h1>
+
+    <h3 class="card__type">
+    ${Json.mimeType}
+    </h3>
+
+    <table class="card__stats">
+      <tbody><tr>
+        <th><i class="fa fa-download"></i> Download</th>
+        <td>95</td>
+      </tr>
+      <tr>
+        <th><i class="fa fa-eye"></i> Watched</th>
+        <td>65</td>
+      </tr>
+      
+      <tr>
+        <th><i class="fa fa-star"></i> Rating</th>
+        <td>110</td>
+      </tr>
+
+    </tbody></table>
+    
+    <div class="card__abilities">
+      <h4 class="card__ability">
+        <button class="btn btn-secondary card__label"><i class="fa fa-download"></i> Download</button>
+        
+      </h4>
+      <h4 class="card__ability">
+        <button class="btn btn-secondary card__label"><i class="fa fa-eye"></i> Watch    </button>
+      </h4>
+    </div>
+  </figcaption>
+</figure>
   `
   return html;
 }
