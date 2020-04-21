0a840710eb4e96afc31141b0fe01ccb318e713ec
ThusharaSampath <59133724+ThusharaSampath@users.noreply.github.com>
2020-04-21 14:34:38 +0530

    2.34

--- public/javascripts/filmCard.js
+++ public/javascripts/filmCard.js
@@ -1,15 +1,14 @@
-import { json } from "body-parser"
+
 
 function test() {
+
   
-  var txt = '{"id":"1VQT1-F8mI4MZ77Z7bM6IlFaz0JeujMsn","username":"downloadto2drive@gmail.com","name":"like.a.boss.2020.720p.bluray.x264.aac-[yts.mx].mp4","size":"803084572","thumbnail":"https://lh3.googleusercontent.com/rGInwhnPEK2F1gxDKcGI5c66TQnmVBCCSKSCQBJ6T4HTamLaZ0p_4Gv0tP2p4Okf4yLHRAgbUFk=s220","url_view":"https://drive.google.com/file/d/1VQT1-F8mI4MZ77Z7bM6IlFaz0JeujMsn/view?usp=drivesdk","url":"https://drive.google.com/uc?id=1VQT1-F8mI4MZ77Z7bM6IlFaz0JeujMsn&export=download","mimeType":"video/mp4","email":"downloadto2drive@gmail.com","owner":"download2 drive"}'
   var j =JSON.parse(txt);
-  makecard(j)
+  makeAcard(j)
 }
 
 
-function makecard(Json) {
-th
+function makeAcard(Json) {
   html = `
   <div class="container" style="background-image: url(${Json.thumbnail})">
   <div class="overlay">
@@ -31,5 +30,5 @@ th
   <h5 class="tumbName" id="thumbName">${Json.name}</h5>
  </div> 
   `
-  console.log("emited")
+  console.log(html);
 }
