472c41b461b1c9a5df288c0ad8ab532846a85405
ThusharaSampath <59133724+ThusharaSampath@users.noreply.github.com>
2020-05-02 23:30:41 +0530

    side nav

---
+++ public/javascripts/nav.js
@@ -0,0 +1,11 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "325px";
+    document.getElementById("content").style.opacity = "1";
+}
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("content").style.opacity = "0";
+  }
